38abbe0cbe4edaff447e20f1f761570fafefd3f9
upright <uprightojieh316@gmail.com>
2026-09-15 13:21:38 +0100

    refine

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -10,9 +10,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = var.vpc_id
-  cidr_block = var.private_subnet_cidr
-  availability_zone = "us-east-1a"
+  vpc_id             = var.vpc_id
+  cidr_block         = var.private_subnet_cidr
+  availability_zone  = "us-east-1a"
 
   tags = {
     Name = "private-subnet"
